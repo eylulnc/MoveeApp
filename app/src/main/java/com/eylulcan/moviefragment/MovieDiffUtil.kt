c39d43f686016433dc694b791acb848314e0ed10
eylulnc <eylulnc@gmail.com>
2021-11-15 18:07:16 +0300

    Transaction Animation added

---
+++ app/src/main/java/com/eylulcan/moviefragment/MovieDiffUtil.kt
@@ -0,0 +1,29 @@
+package com.eylulcan.moviefragment
+
+import androidx.annotation.Nullable
+import androidx.recyclerview.widget.DiffUtil
+
+class MovieDiffUtil<Movie>(
+    private val oldList : List<Movie>,
+    private val newList : List <Movie>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int =newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        println("DiffUtil  old " + oldList[oldItemPosition] + " new " +newList[newItemPosition] )
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+
+    @Nullable
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+        //you can return particular field for changed item.
+        return super.getChangePayload(oldItemPosition, newItemPosition)
+    }
+
+}
