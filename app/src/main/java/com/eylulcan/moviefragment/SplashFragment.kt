d47041e8d9797a1582ba7d621a6004336d60e977
eylulnc <eylulnc@gmail.com>
2021-11-16 13:02:53 +0300

    Splash Fragment added

---
+++ app/src/main/java/com/eylulcan/moviefragment/SplashFragment.kt
@@ -0,0 +1,32 @@
+package com.eylulcan.moviefragment
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =inflater.inflate(R.layout.fragment_splash, container, false)
+        Handler(Looper.myLooper()!!).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_movieListFragment,
+                null, NavOptions.Builder().setPopUpTo(R.id.splashFragment, true).build())
+        },1500)
+        // or update fragment action - app:popUpTo="@id/splashFragment" app:popUpToInclusive="true"
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
