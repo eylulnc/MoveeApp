9033046c4e5c87fa988882a8668b59d4b3fa7cd7
eylulnc <eylulnc@gmail.com>
2022-01-14 14:27:56 +0300

    Menu removed from Dashboard Fragment.

--- app/src/main/java/com/eylulcan/moviefragment/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/eylulcan/moviefragment/ui/dashboard/DashboardFragment.kt
@@ -1,27 +1,19 @@
 package com.eylulcan.moviefragment.ui.dashboard
 
 import android.os.Bundle
-import android.view.*
-import android.widget.Toast
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavOptions
 import androidx.navigation.Navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.eylulcan.moviefragment.R
 import com.eylulcan.moviefragment.databinding.FragmentDashboardBinding
-import com.google.firebase.auth.FirebaseAuth
 
 class DashboardFragment : Fragment() {
 
     private lateinit var binding: FragmentDashboardBinding
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,20 +38,4 @@ class DashboardFragment : Fragment() {
         binding.bottomNavigationView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.logout) {
-            auth.signOut()
-            Toast.makeText(context, R.string.logged_out_movie_list, Toast.LENGTH_LONG).show()
-            findNavController().navigate(
-                R.id.action_dashboardFragment_to_loginFragment, null,
-                NavOptions.Builder().setPopUpTo(R.id.dashboardFragment, true).build()
-            )
-        }
-        return super.onOptionsItemSelected(item)
-    }
 }
