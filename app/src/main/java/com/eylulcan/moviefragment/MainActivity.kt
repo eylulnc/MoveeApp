1d4edaad4c228332b882a13baa510de3e972eb93
eylulnc <eylulnc@gmail.com>
2022-01-14 14:26:55 +0300

    Splash Fragment removed. Instead of Splash fragment another solution applied for splash screen.

--- app/src/main/java/com/eylulcan/moviefragment/MainActivity.kt
+++ app/src/main/java/com/eylulcan/moviefragment/MainActivity.kt
@@ -1,16 +1,58 @@
 package com.eylulcan.moviefragment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
 import com.eylulcan.moviefragment.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val auth: FirebaseAuth = Firebase.auth
+    lateinit var sharedPreferences: SharedPreferences
+    private var firstTimeOpened: Boolean? = null
+    private lateinit var navGraph: NavGraph
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.SplashScreen)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        sharedPreferences = this.getSharedPreferences(
+            getString(R.string.app_package_name),
+            Context.MODE_PRIVATE
+        )
+        setUpUI()
     }
+
+    private fun setUpUI() {
+        val navHostFragment =
+            binding.fragmentContainerView.getFragment<Fragment>() as NavHostFragment
+        val graphInflater = navHostFragment.navController.navInflater
+        navGraph = graphInflater.inflate(R.navigation.navigation_graph)
+        navController = navHostFragment.navController
+        val currentUser = auth.currentUser
+        currentUser?.let {
+            navGraph.startDestination = R.id.dashboardFragment
+        } ?: run {
+            firstTimeOpened = sharedPreferences.getBoolean(getString(R.string.isFirst), true)
+            if (firstTimeOpened == true) {
+                sharedPreferences.edit().putBoolean(getString(R.string.isFirst), false).apply()
+                navGraph.startDestination = R.id.onBoardViewPagerFragment
+            } else {
+                navGraph.startDestination = R.id.loginFragment
+            }
+            navController.graph = navGraph
+        }
+    }
+
 }
