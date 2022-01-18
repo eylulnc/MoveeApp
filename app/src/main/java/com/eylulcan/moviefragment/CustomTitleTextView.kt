f5974d3e2f5d80e7627be6283e1b97d4e1d127b6
eylulnc <eylulnc@gmail.com>
2022-01-18 10:54:31 +0300

    CustomView Class Created.

--- app/src/main/java/com/eylulcan/moviefragment/CustomTitleTextView.kt
+++ app/src/main/java/com/eylulcan/moviefragment/CustomTitleTextView.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.graphics.Canvas
 import android.text.TextUtils
 import android.util.AttributeSet
+import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 
 private const val DEFAULT_TEXT_SIZE = 20F
+private const val LINES = 2
+
 class CustomTitleTextView : androidx.appcompat.widget.AppCompatTextView {
     constructor(context: Context) : super(context) {
         val typeface = ResourcesCompat.getFont(context, R.font.lato_bold)
@@ -14,7 +17,7 @@ class CustomTitleTextView : androidx.appcompat.widget.AppCompatTextView {
         //val face = Typeface.createFromAsset(context.assets, R.font.lato_bold.toString())
         this.typeface = typeface
         this.setTextColor(resources.getColor(R.color.white_light_grey))
-        this.setLines(2)
+        this.setLines(LINES)
         this.ellipsize = TextUtils.TruncateAt.END
     }
 
@@ -23,7 +26,7 @@ class CustomTitleTextView : androidx.appcompat.widget.AppCompatTextView {
         this.typeface = typeface
         this.textSize = DEFAULT_TEXT_SIZE
         this.setTextColor(resources.getColor(R.color.white_light_grey))
-        this.setLines(2)
+        this.setLines(LINES)
         this.ellipsize = TextUtils.TruncateAt.END
     }
 
@@ -36,11 +39,13 @@ class CustomTitleTextView : androidx.appcompat.widget.AppCompatTextView {
         this.typeface = typeface
         this.textSize = DEFAULT_TEXT_SIZE
         this.setTextColor(resources.getColor(R.color.white_light_grey))
-        this.setLines(2)
+        this.setLines(LINES)
         this.ellipsize = TextUtils.TruncateAt.END
+
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
     }
+
 }
--- app/src/main/java/com/eylulcan/moviefragment/ui/artistdetail/ArtistMovieAdapter.kt
+++ app/src/main/java/com/eylulcan/moviefragment/ui/artistdetail/ArtistMovieAdapter.kt
@@ -1,7 +1,9 @@
 package com.eylulcan.moviefragment.ui.artistdetail
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.eylulcan.moviefragment.ItemListener
@@ -9,12 +11,20 @@ import com.eylulcan.moviefragment.R
 import com.eylulcan.moviefragment.databinding.ArtistMovieFragmentRecyclerRowBinding
 import com.eylulcan.moviefragment.model.ArtistMovieCredits
 import com.eylulcan.moviefragment.util.Utils
+import me.samlss.broccoli.Broccoli
+import java.util.HashMap
 
 class ArtistMovieAdapter(
     private val movieCredits: ArtistMovieCredits,
     private val artistMovieClickListener: ItemListener
 ) :
     RecyclerView.Adapter<ArtistMovieAdapter.ViewHolder>() {
+
+    private val mViewPlaceholderManager: HashMap<View, Broccoli> = HashMap<View, Broccoli>()
+    private val mTaskManager: HashMap<View, Runnable> = HashMap<View, Runnable>()
+
+    private val broccoli = Broccoli()
+    private val placeholderNeeded = arrayListOf<View>()
 
     class ViewHolder(val binding: ArtistMovieFragmentRecyclerRowBinding) :
         RecyclerView.ViewHolder(binding.root)
@@ -30,6 +40,12 @@ class ArtistMovieAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var broccoli = mViewPlaceholderManager[holder.itemView]
+        if (broccoli == null) {
+            broccoli = Broccoli()
+            mViewPlaceholderManager[holder.itemView] = broccoli
+        }
+        setPlaceholders(holder)
         val movie = movieCredits.cast?.get(position)
         holder.binding.movieNameArtistMovie.text = movie?.title
         Glide.with(holder.binding.root).load(setImageUrl(movie?.posterPath))
@@ -37,6 +53,18 @@ class ArtistMovieAdapter(
             .into(holder.binding.movieImage)
         holder.itemView.setOnClickListener {
             movie?.id?.let { id -> artistMovieClickListener.onItemClicked(id) }
+            var task: Runnable? = mTaskManager[holder.itemView]
+            if (task == null) {
+                task = Runnable {
+                    run {
+                        removePlaceholders()
+                    }
+                }
+                mTaskManager[holder.itemView] = task
+            } else {
+                holder.itemView.removeCallbacks(task)
+            }
+            holder.itemView.postDelayed(task, 2000)
         }
     }
 
@@ -44,5 +72,24 @@ class ArtistMovieAdapter(
 
     private fun setImageUrl(poster_path: String?): String =
         Utils.BASE_IMAGE_URL_ORIGINAL.plus(poster_path)
+
+    private fun setPlaceholders(holder:ViewHolder) {
+        placeholderNeeded.addAll(
+            arrayListOf(
+                holder.binding.artistMovieRowTemplate
+            )
+        )
+        Utils.addPlaceholders(broccoli = broccoli, placeholderNeeded)
+    }
+
+    private fun removePlaceholders() {
+        placeholderNeeded.forEach { view ->
+            view.apply {
+                broccoli.clearPlaceholder(this)
+                this.isVisible = false
+            }
+        }
+    }
+
 
 }
