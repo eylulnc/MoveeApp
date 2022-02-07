e24f4a5b351c5cce9658ea0bcbff3803dfe8dbf5
eylulnc <eylulnc@gmail.com>
2022-02-07 11:36:35 +0300

    data/Utils updated.

--- data/src/main/java/com/eylulcan/moviefragment/data/Utils.kt
+++ data/src/main/java/com/eylulcan/moviefragment/data/Utils.kt
@@ -4,5 +4,6 @@ object Utils {
     const val IF_INT_NULL = 0
     const val IF_STR_NULL = ""
     const val IF_BOOLEAN_NULL = false
-    const val If_DBL_NULL = 0.0
+    const val IF_DBL_NULL = 0.0
+    const val IF_LONG_NULL = 0L
 }
