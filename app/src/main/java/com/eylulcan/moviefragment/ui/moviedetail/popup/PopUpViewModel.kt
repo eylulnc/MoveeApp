09fa0958d6ca078d08f32971808173c5ebc13435
eylulnc <eylulnc@gmail.com>
2022-02-07 10:30:39 +0300

    ViewModel updated.

--- app/src/main/java/com/eylulcan/moviefragment/ui/moviedetail/popup/PopUpViewModel.kt
+++ app/src/main/java/com/eylulcan/moviefragment/ui/moviedetail/popup/PopUpViewModel.kt
@@ -3,9 +3,9 @@ package com.eylulcan.moviefragment.ui.moviedetail.popup
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.eylulcan.moviefragment.api.MovieAPI
-import com.eylulcan.moviefragment.model.PostRatingBody
-import com.eylulcan.moviefragment.model.RatingPostResponse
+import com.eylulcan.moviefragment.domain.entity.PostRatingBodyEntity
+import com.eylulcan.moviefragment.domain.entity.RatingPostResponseEntity
+import com.eylulcan.moviefragment.domain.usecase.PostRatingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -13,26 +13,22 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PopUpViewModel @Inject constructor(private var retrofit: MovieAPI): ViewModel() {
+class PopUpViewModel @Inject constructor(private val postRatingUseCase: PostRatingUseCase) :
+    ViewModel() {
 
-    private val ratingResponse = MutableLiveData<RatingPostResponse>()
-    val responseRating: LiveData<RatingPostResponse> get() = ratingResponse
+    private val ratingResponse = MutableLiveData<RatingPostResponseEntity>()
+    val responseRating: LiveData<RatingPostResponseEntity> get() = ratingResponse
 
     fun postMovieRating(movieID: Int, rate: Double, sessionId: String) {
-        val postRatingBody = PostRatingBody()
-        postRatingBody.ratingValue = rate
+        val postRatingBody = PostRatingBodyEntity(rate)
         CoroutineScope(Dispatchers.IO).launch {
-            val response = retrofit.postMovieRating(
-                movieID = movieID,
-                postRatingBody = postRatingBody,
-                sessionId = sessionId
+            val response = postRatingUseCase.invoke(
+                movieId = movieID,
+                sessionId = sessionId,
+                postBody = postRatingBody
             )
             response.let {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        ratingResponse.postValue(it)
-                    }
-                }
+                ratingResponse.postValue(it)
             }
         }
     }
