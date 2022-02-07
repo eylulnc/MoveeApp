d824c45b2e4f2208d413aa7fbb0e72396ac26c03
eylulnc <eylulnc@gmail.com>
2022-02-07 11:38:58 +0300

    ArtistMapper updated.

--- data/src/main/java/com/eylulcan/moviefragment/data/mapper/ArtistMapper.kt
+++ data/src/main/java/com/eylulcan/moviefragment/data/mapper/ArtistMapper.kt
@@ -3,7 +3,7 @@ package com.eylulcan.moviefragment.data.mapper
 import com.eylulcan.moviefragment.data.Utils.IF_BOOLEAN_NULL
 import com.eylulcan.moviefragment.data.Utils.IF_INT_NULL
 import com.eylulcan.moviefragment.data.Utils.IF_STR_NULL
-import com.eylulcan.moviefragment.data.Utils.If_DBL_NULL
+import com.eylulcan.moviefragment.data.Utils.IF_DBL_NULL
 import com.eylulcan.moviefragment.data.model.ArtistAlbum
 import com.eylulcan.moviefragment.data.model.ArtistDetail
 import com.eylulcan.moviefragment.data.model.ArtistList
@@ -55,7 +55,7 @@ class ArtistMapper @Inject constructor() {
             knownForDepartment = artistDetail.knownForDepartment ?: IF_STR_NULL,
             name = artistDetail.name ?: IF_STR_NULL,
             placeOfBirth = artistDetail.placeOfBirth ?: IF_STR_NULL,
-            popularity = artistDetail.popularity ?: If_DBL_NULL,
+            popularity = artistDetail.popularity ?: IF_DBL_NULL,
             profilePath = artistDetail.profilePath ?: IF_STR_NULL
 
         )
